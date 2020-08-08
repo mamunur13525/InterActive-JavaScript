584ee29e8c4e9eba2ac5d1ca04ba63e6353fbd76
mamunur13525 <How would you declare an arrow function with no parameter?>
2020-08-08 17:57:23 +0600

    done

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+
+let name ;
+console.log(name);
+
+
+function ages(num1, num2){
+    num1 + num2
+    return ;
+}
+console.log(ages(21, 23));
+
+
+ let array = [12, 13, 25];
+ console.log(array[15]);
+
+
+let objects = {name: "mamun", roll: 12, friend: "masud"};
+console.log(objects.gf);
+
+
+let name = undefined;
+console.log(name);
--- true-false.js
+++ true-false.js
@@ -1,1 +1,21 @@
-thats
+//Falsy
+// 0 
+// ""
+//false
+// undefined
+// null
+// NaN
+
+//Truly
+// "0", :" ", true, [], {}
+
+
+let name = -5;
+let friend = " ";
+
+
+if(friend){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
